4ada581ecb6abdb5d97f3ca0390c2010f304ac0f
ASHUTOSH <142031758+AASHUTOSHgautam@users.noreply.github.com>
2023-09-10 09:27:57 +0530

    uploaded successfully
    saved Files

---
+++ index.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio</title>
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+     
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- <script src="js/script.js"></script> -->
+</head>
+
+<body>
+    <header>
+        <section style="padding-top: 0%;">
+            <div class="container">
+                <nav id="main-nav" class="flex items-center justify-between">
+                    <div class="left flex items-center">
+                        <div class="branding">
+                            <a href="#home"> <img src="./images/infinityicon.png" alt="infinityicon"></a>
+                        </div>
+                        <div>
+                            <a href="#">Home</a>
+                            <a href="#about">About</a>
+                            <a href="#Education">Education</a>
+                            <a href="#Projects">Projects</a>
+                            <a href="#Blogs">Blogs</a>
+                        </div>
+                    </div>
+                    <div class="right">
+                        <a href="#contact"><button class="btn btn-primary">Contact</button></a>
+                    </div>
+                </nav>
+                <div class="hero flex items-center justify-between">
+                    <div class="left flex-1 flex justify-center">
+                        <img src="./images/cvpic.png" alt="">
+                    </div>
+                    <div class="right flex-1"><br><br><br><br>
+                        <h6>ASHUTOSHgautam</h6>
+                        <h1>I’m a Full Stack<span> Developer</span></h1>
+                        <p>Passionate Full Stack Developer with a flair for front-end design and back-end development.
+                            Crafting seamless digital experiences with cutting-edge technologies. Problem solver and
+                            user-centric thinker. Committed to delivering high-performance web applications.</p>
+                        <div>
+                           <a href="#explore"> <button class="btn btn-secondary">DOWNLOAD CV</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </header>
+
+    <section id="about" class="about">
+        <div class="container flex items-center about-inner-wrap">
+            <div class="flex-1">
+                <img class="about-me-img" src="./images/p5.jpg" alt="">
+            </div>
+            <div id="white-space" class="flex-1 right">
+                <h1>About <span>Me</span></h1>
+                <h3>Hello! i’m ASHUTOSHgautam</h3>
+                <p>
+                    1. Enthusiastic Full Stack Developer with expertise in both front-end and back-end technologies.
+                    2. Dedicated to building scalable and responsive web applications that deliver outstanding user
+                    experiences.
+                    3. Proficient in various programming languages and frameworks, ensuring efficient and reliable
+                    development.
+                    4. Continuously exploring new technologies to stay at the forefront of the ever-evolving web
+                    landscape.
+                </p>
+
+                <div class="social">
+                    <a href="https://www.linkedin.com/feed/"><img src="./images/linkdinmain.png"
+                            alt="linkdin image"></a>
+                    <a href="https://www.facebook.com/profile.php?id=100053731551072"><img src="./images/facebook.png"
+                            alt="facebook image"></a>
+                    <a href="https://www.instagram.com/ashutoshgautam11/saved/all-posts/"> <img
+                            src="./images/instagram.png" alt="instagram image"></a>
+                    <a href="https://github.com/AASHUTOSHgautam/NumberGuessing.git"><img src="./images/GitHub.png"
+                            alt="Github image"></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="Education" class="services">
+        <div class="container">
+            <h1 class="section-heading"><span>Education</span> Qualification & <span>IT</span> Proficiency</h1>
+            <p>"Empowering minds through the gift of education."</p>
+            <div class="card-wrapper">
+                <div class="card">
+                    <img src="./images/brush.svg" alt="card icon">
+                    <h2>Bachelor of Computer Science (2020-2024)</h2>
+                    <p>
+                        "Tech enthusiast pursuing a Bachelor's in Computer Science, driven to explore the endless
+                        possibilities of code."
+                    </p>
+                </div>
+                <div class="card">
+                    <img src="./images/brush.svg" alt="card icon">
+                    <h2>Senior Secondary Level &nbsp (2019-2020)</h2>
+                    <p>"Inquisitive 12th-grade PCM student with a thirst for knowledge in Physics, Chemistry,
+                        and Mathematics, aspiring to make a meaningful impact in the realm of scientific exploration and
+                        research."
+                    </p>
+                </div>
+                <div class="card">
+                    <img src="./images/brush.svg" alt="card icon">
+                    <h2>Primary Level (2017-2018)</h2>
+                    <p>"Aspiring 10th-grade student with a hunger for learning and a passion for personal growth,
+                        ready to embrace the challenges that lead to a brighter future."
+                    </p>
+                </div>
+
+                <div class="card">
+                    <img src="./images/brush.svg" alt="card icon">
+                    <h2>Web Development</h2>
+                    <p>Mastering of <b>HTML5, CSS, JavaScript, Bootstrap and Databases, </b> I sculpt captivating
+                        websites that harmonize structure, style,
+                        and interactivity. With a keen eye for detail, I transform ideas into dynamic online realities.
+                    </p>
+                </div>
+                <div class="card">
+                    <img src="./images/brush.svg" alt="card icon">
+                    <h2>Java + DSA</h2>
+                    <p>Java, synergized with Data Structures and Algorithms, forms a dynamic duo for creating
+                        high-performance software solutions through optimized data handling and strategic
+                        problem-solving.
+                    </p>
+                </div>
+
+                <div class="card">
+                    <img src="./images/brush.svg" alt="card icon">
+                    <h2> AI & ML</h2>
+                    <p>Infusing the power of AI and ML into web applications using HTML, CSS, and JavaScript, I create
+                        intelligent interfaces that adapt and learn from user
+                        interactions. By blending coding finesse with cutting-edge technologies, I build bridges between
+                        human creativity and machine intelligence.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section id="explore" class="explore">
+        <h1>Always ready for exploring the new ideas.</h1>
+        <p>Java with DSA || MySQL Basic || JDBC || Web Development(HTML/CSS/JS/Bootstrap/DB)</p>
+        <button class="btn btn-primary"> Explore CV</button>
+    </section>
+    <section class="reviews" id="Projects">
+        <div class="container">
+            <h1 class="section-heading"><span>Projects </span>Work</h1>
+            <p>My IT project portfolio mirrors a chronicle of visionary solutions, where code meets creativity to
+                engineer transformative digital experiences.</p>
+            <div class="slider">
+                <div class="slide">
+                    <img src="./images/p5.jpg" alt="ashutosh image">
+                    <p> "The voting system project exemplifies technological innovation harnessed for democratic
+                        empowerment, fostering transparent and efficient electoral processes."</p>
+                    <span><b>- Voting System using web technology tools </b></span>
+                </div>
+                <div class="slide">
+                    <img src="./images/p5.jpg" alt="">
+                    <p> "The college management system project streamlines academic administration through a digital
+                        interface, optimizing communication and resource allocation."</p>
+                    <span><b>- College Management System using web tools</b></span>
+                </div>
+                <div class="slide">
+                    <img src="./images/p5.jpg" alt="">
+                    <p> The chatbot project seamlessly merges AI and user interaction, creating a dynamic virtual
+                        assistant for personalized and efficient communication."</p>
+                    <span><b>-ChatBot using Pyhon</b></span>
+                </div>
+                <div class="slide">
+                    <img src="./images/p5.jpg" alt="">
+                    <p>"The ATM system project epitomizes user-centric design, facilitating secure and convenient
+                        financial transactions through a technology-driven interface."</p>
+                    <span><b>-Banking System or ATM System using C & Graphics
+                        </b></span>
+                </div>
+            </div>
+            <div class="slider-dots"></div>
+        </div>
+    </section>
+
+    <section id="Blogs" class="blog">
+        <div class="container">
+            <h1 class="section-heading"><span>MY</span> Blogs</h1>
+            <p>"Embarking on a journey of innovation, a CSE student explores the digital frontier, transforming
+                knowledge into impactful solutions."</p>
+
+            <div class="card-wrapper">
+                <div class="card">
+                    <div class="img-wrapper">
+                        <img src="./images/webpic.jpg" alt="">
+                    </div>
+                    <div class="card-content">
+                        <a href="webdevlopmentblog.html">
+                            <h1>Web development thought</h1>
+                        </a>
+                        <span>Sep 10, 2023</span>
+                        <p>Web development is a dynamic field that involves creating and maintaining websites, ranging
+                            from simple static pages to complex web applications.
+
+                            It encompasses various technologies such as HTML, CSS, JavaScript, and more, allowing
+                            developers to craft engaging user experiences.
+
+                            Through responsive design, interactivity, and efficient coding practices, web developers
+                            shape the digital landscape, driving innovation and accessibility.
+
+                            Whether you're a novice eager to learn or a seasoned pro refining your skills, the world of
+                            web development offers endless opportunities for growth.</p>
+                        <a href="webdevlopmentblog.html">Read More</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img-wrapper">
+                        <img src="./images/aimlpic.png" alt="">
+                    </div>
+                    <div class="card-content">
+                        <a href="AI&MLblog.html">
+                            <h1>Artificial Intelligence & Machine Learning</h1>
+                        </a>
+                        <span>Sep 12, 2023</span>
+
+                        <p><br>Artificial Intelligence, or AI, is a transformative technology that enables machines to
+                            mimic human cognitive functions.
+                            Through machine learning algorithms and neural networks, AI systems can analyze vast amounts
+                            of data, recognize patterns, and make intelligent decisions.
+                            From autonomous vehicles to medical diagnostics, AI is reshaping industries and offering
+                            unprecedented possibilities for automation and problem-solving.
+                            As AI continues to evolve, it holds the potential to revolutionize how we interact with
+                            technology and enhance the quality of our lives.</p>
+                        <a href="AI&MLblog.html">Read More</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img-wrapper">
+                        <img src="./images/javamainpic.png" alt="">
+                    </div>
+                    <div class="card-content">
+                        <a href="javablog.html">
+                            <h1>Java Development</h1>
+                        </a>
+                        <span>Sep 15, 2023</span>
+                        <p>Java is a versatile programming language renowned for its portability, making it a foundation
+                            for building a wide range of applications.
+
+                            From enterprise software to Android app development, Java's robustness and comprehensive
+                            libraries empower developers to create efficient and reliable solutions.
+
+                            By harnessing its object-oriented principles and extensive ecosystem, Java developers shape
+                            the digital landscape with creativity and innovation.
+
+                            Embark on a Java development journey to unlock a world of opportunities for crafting
+                            cutting-edge software.</p>
+                        <a href="javablog.html">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="contact" class="contact">
+        <div class="container">
+            <h1 class="section-heading">Contact <span>Us</span></h1>
+            <p>We provide high standar clean website for your business solutions</p>
+            <div class="card-wrapper">
+                <div class="card">
+                    <img src="./images/phonesvg.svg" alt="">
+                    <h1>Call Us On</h1>
+                    <h6>+91 9411695639</h6>
+                </div>
+                <div class="card">
+                    <img src="./images/messages.svg" alt="">
+                    <h1>Email Us At</h1>
+                    <h6>ashutoshkumar6032002@gmail.com</h6>
+                </div>
+                <div class="card">
+                    <img src="./images/mapsvg.svg" alt="">
+                    <h1>Placed location</h1>
+                    <h6>Vidur kuti road ,Bijnor || pincode- 246701, Uttar Pradesh (india)</h6>
+                </div>
+
+            </div>
+            <br>
+          <h1 style="text-align: center;">Send Your Message</h1><br><br>
+            <form method="post" action="#">
+                <div class="input-wrap">
+                    <input type="text" placeholder="Your Name *" name="nm">
+                    <input type="email" placeholder="Your Email *" name="em">
+                </div>
+                <div class="input-wrap-2">
+                    <input type="text" placeholder="Your Subject..." name="sm">
+                    <textarea name="text" id="" cols="30" rows="10" placeholder="Your Message..."></textarea>
+                </div>
+                <div class="btn-wrapper">
+                    <button class="btn btn-primary" name="sb">Send Message</button>
+                </div>
+            </form>
+       
+            <?php
+                $conn=mysqli_connect("localhost","root",'',"form");
+                if(!$conn){
+                    die("faield to connect to a database ERROR:".mysqli_connect_errno());
+
+                }
+                if (isset($_POST['sb']))
+                {
+                    $name=$_POST['nm'];
+                    $email=$_POST['em'];
+                    $subject=$_POST['sm'];
+                    $message=$_POST['text'];
+
+                    $sql_query="INSERT INTO `details`(`name`,`email`,`subject`,`message`) VALUES ('$name','$email','$subject','$message')";
+                    if(mysqli_query($conn,$sql_query))
+                    {
+                        echo "Error";
+                    }
+                    else{
+                        echo "Error". $sql."". mysqli_error($conn);
+                    }
+
+                     
+                }
+                ?>
+
+        </div>
+    </section>
+    <footer>
+        <img class="footer-logo" src="./images/infinityicon.png" alt="">
+        <div class="footer-socials">
+            <a href="https://www.linkedin.com/feed/"><img src="./images/linkdinmain.png" alt="linkdin image"></a>
+            <a href="https://www.facebook.com/photo/?fbid=724924332641945&set=a.311778723956510"><img
+                    src="./images/facebook.png" alt="facebook image"></a>
+            <a href="https://www.instagram.com/ashutoshgautam11/saved/all-posts/"> <img src="./images/instagram.png"
+                    alt="ubstagran image"></a>
+            <a href="https://github.com/AASHUTOSHgautam/NumberGuessing.git"> <img src="./images/githubmain.png"
+                    alt="GitHub image"></a>
+
+        </div>
+        <div class="copyright">
+            Thanks for visiting Infinity TechSkill
+        </div>
+    </footer>
+    </div>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script src="./js/app.js"></script>
+
+</body>
+
+</html>
